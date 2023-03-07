4cd8fc30d2bf8a55fe2f71c57aa68efc916f2c8e
emily <119440810+t4nger1ne@users.noreply.github.com>
2023-03-07 12:26:23 -0500

    added in patient ability to teleport to the office when called

--- Assets/Scripts/Movement/Patient.cs
+++ Assets/Scripts/Movement/Patient.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Patient : MonoBehaviour
 {
-    public CharacterController patient1Controller;
+    public CharacterController patientController;
     public GameObject officeSpawn;
     private bool patientMustMove = true;
 
@@ -18,11 +18,14 @@ public class Patient : MonoBehaviour
     void Update()
     {
         if (patientMustMove) {
-                if (Input.GetKeyDown(KeyCode.E)) {
-                    Debug.Log("hi");
-                    gameObject.transform.position = officeSpawn.transform.position;
-                    transform.Rotate(0, 90f, 0);
+                if (patientController.tag == "currentPatient") {
+                     if (Input.GetKeyDown(KeyCode.E)) {
+                        Debug.Log("hi");
+                        gameObject.transform.position = officeSpawn.transform.position;
+                        // transform.Rotate(0, 10f, 0);
+                    }
                 }
+
         }
     }
 }
